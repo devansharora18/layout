4ce4a4cff30ec82fec826a51731f821f3947effb
Devansh Arora <hsnaved.reverse@gmail.com>
2025-09-20 02:01:20 +0530

    feat: added basic canvas with div

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Canvas from "@/components/canvas/Canvas";
+
 export default function Home() {
   return (
-    <main className="flex items-center justify-center h-screen">
+    <main className="flex flex-col items-center justify-center h-screen gap-6">
       <h1 className="text-3xl font-bold">Layout</h1>
+      <Canvas />
     </main>
   );
 }
---
+++ components/canvas/Canvas.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Canvas() {
+  return (
+    <div className="w-4/5 h-96 border bg-gray-50 flex items-center justify-center">
+      <div className="w-full h-full bg-blue-400 flex items-center justify-center text-white font-bold">
+        Div
+      </div>
+    </div>
+  );
+}
