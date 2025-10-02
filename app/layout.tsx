7ebfb2abafba37ee776bee392bc272d12da430a1
Devansh Arora <hsnaved.reverse@gmail.com>
2025-10-03 05:09:08 +0530

    feat: added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import "./globals.css";
 import Providers from "./providers";
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 
 export const metadata = {
   title: "Layout",
@@ -7,11 +9,13 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const GTAG = process.env.GTAG || "";
   return (
     <html lang="en">
       <body>
         <Providers>{children}</Providers>
       </body>
+      <GoogleAnalytics gaId={GTAG}/>
     </html>
   );
 }
